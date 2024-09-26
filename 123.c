07a4ab45c27846cfeb6a30eb435becef1b1981a0
andyhihello <andyhung3356@gmail.com>
2024-09-26 10:52:02 +0800

    Update 123.c

--- 123.c
+++ 123.c
@@ -3,13 +3,30 @@
 #include <math.h>
 
 int main(){
-    int version,people,daily,actcount,actget,need,total;
+    long long version,people,daily,actcount,actget,need,total;
     scanf("%d %d %d %d %d",&version,&people,&daily,&actcount,&actget);
-    int canget = version*actcount*actget;
+    long long canget = version*actcount*actget;
     if (daily){
         canget = canget + version*42*60;
     }
     need = (7-people)*160*160;
-    total = (need - canget)/2;
+    total = (need - canget)/2 + ((need - canget)%2) ;
+    if (total <= 0){
+        printf("It's enough");
+        return 0;
+    }
+    else if (total <= 1000){
+        printf("%d For wanderer, it worth.",total);
+       
+        return 0;
+    }
+    else if (total <= 5000){
+        printf("%d Barely ok",total);
+        return 0;
+    }
+    else {
+        printf("%d So love will disappear, right?",total);
+        return 0; 
+    }
 
 }
