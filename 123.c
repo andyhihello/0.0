da55f16253d1cf29272e56515fad366e66c3cb97
andyhihello <andyhung3356@gmail.com>
2024-10-29 11:25:34 +0800

    Update 123.c
    13

--- 123.c
+++ 123.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+
+long long factorial(int n){
+    if (n==0){
+        return 1;
+    }
+    else if (n==1){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+    }
+
+}
 
 int main(){
-    long long version,people,daily,actcount,actget,need,total;
-    scanf("%d %d %d %d %d",&version,&people,&daily,&actcount,&actget);
-    long long canget = version*actcount*actget;
-    if (daily){
-        canget = canget + version*42*60;
-    }
-    need = (7-people)*160*160;
-    total = (need - canget)/2 + ((need - canget)%2) ;
-    if (total <= 0){
-        printf("It's enough");
-        return 0;
-    }
-    else if (total <= 1000){
-        printf("%d For wanderer, it worth.",total);
-       
-        return 0;
-    }
-    else if (total <= 5000){
-        printf("%d Barely ok",total);
-        return 0;
-    }
-    else {
-        printf("%d So love will disappear, right?",total);
-        return 0; 
+    long long n,m,t;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%lld %lld",&n,&m);
+        printf("%lld",)
     }
 
 }
